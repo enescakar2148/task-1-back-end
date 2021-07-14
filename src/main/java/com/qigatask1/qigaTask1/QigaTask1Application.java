b8c680759e4381a2ff706a99447ebce16cb49970
Enes Çakar <e.52nes@hotmail.com>
2021-07-14 23:00:53 +0300

    create Spring Boot Application

---
+++ src/main/java/com/qigatask1/qigaTask1/QigaTask1Application.java
@@ -0,0 +1,13 @@
+package com.qigatask1.qigaTask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QigaTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QigaTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qigatask1/qigaTask1/QigaTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qigatask1.qigaTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QigaTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
