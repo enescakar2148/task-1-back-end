6904c985b3ec2ed4e011e64a160202265f511f3f
Enes Çakar <e.52nes@hotmail.com>
2021-07-19 20:10:23 +0300

    post process succes

--- src/main/java/com/qigatask1/qigaTask1/QigaTask1Application.java
+++ src/main/java/com/qigatask1/qigaTask1/QigaTask1Application.java
@@ -2,12 +2,8 @@ package com.qigatask1.qigaTask1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
- //@ComponentScan({ "com.qigatask1.qigaTask1.*" })
 public class QigaTask1Application {
 
 	public static void main(String[] args) {
